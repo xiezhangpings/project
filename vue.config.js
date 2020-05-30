2f4cb7c5023d9d21cd0bbfbf0e041e36ccb66d2f
xiezhangping <1912462430@qq.com>
2020-05-30 17:09:51 +0800

    首页头部

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      postcss: {
+        // 这里的选项会传递给 postcss-loader
+        plugins: [
+          require('postcss-px2rem')({
+            remUnit: 100, // 1rem等于多少px
+          })
+        ]
+      }
+    }
+  },
+  devServer: {
+    overlay: {
+      warnings: false,
+      errors: false
+    }
+  }
+}
